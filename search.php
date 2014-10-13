7c7032454fa3e11aa4cd140a9df2358d09b340c1
Thimo Hagen <55443@carmelcollegegouda.nl>
2014-10-13 19:09:58 +0200

    Added code
    My coding skills may not be perfect, but it's working! :)

---
+++ search.php
@@ -0,0 +1,28 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body bgcolor="#222">
+<style media="screen" type="text/css">
+a {
+    color: red;
+    text-decoration: none;
+}
+</style>
+<font color='white'>
+<center>
+<form action="search.php" method="post">
+<input type="text" placeholder="Enter word" name="word"><br>
+<button name="name" value="Search" type="submit">Search</button>
+</form>
+</body>
+</html>
+<?php
+
+$word = $_POST["word"]; 
+
+echo "Search definition of: <a href='http://dictionary.cambridge.org/dictionary/british/".$word."'>".$word."</a>";
+
+echo "<br /><iframe src='http://dictionary.cambridge.org/dictionary/british/".$word."' width='275' height='450' scrolling='yes' frameBorder='0'></iframe>";
+
+echo "<br /><iframe src='http://www.vertalen.nu/vertaal?vertaal=".$word."&van=en&naar=nl' width='275' height='435' scrolling='yes' frameBorder='0'>Browser not compatible.</iframe>";
+
+?>
